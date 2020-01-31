63e06b0d032bac1229f219b91dfe5fef98d623b7
Karim <taha0025@algonquinlive.com>
2020-01-30 20:09:04 -0500

    * added weatherapi.py

---
+++ Desktop/Python NLTK test/weatherapi.py
@@ -0,0 +1,35 @@
+from win10toast import ToastNotifier
+from apscheduler.schedulers.blocking import BlockingScheduler
+import requests
+import json
+
+scheduler = BlockingScheduler()
+api_key = "1457a7a5dc3881341c72cf558dcd5b12"
+base_url = "http://api.openweathermap.org/data/2.5/weather?"
+interval = 1
+
+def weather_check():
+    city_name = "Ottawa"
+    complete_url = base_url + "appid=" + api_key + "&q=" + city_name
+    response = requests.get(complete_url)
+    x = response.json()
+    toaster = ToastNotifier()
+
+    if x["cod"] != "404":
+        y = x["main"]
+        current_temperature = y["temp"]
+        current_temperature = round(current_temperature - 273.15)
+
+        feels_like = y["feels_like"]
+        feels_like = round(feels_like - 273.15)
+
+        z = x["weather"]
+        weather_description = z[0]["description"]
+
+        toaster.show_toast("Weather Report: " + city_name, "Tempearture = " + str(current_temperature) + "*C\nFeels like  = " + str(feels_like) + "*C\nDescription = " + str(weather_description) + "\nNext Update in: " + str(interval) + " hour(s)")
+
+    else:
+        print("Nothing dummy")
+
+scheduler.add_job(weather_check, 'interval', hours=interval)
+scheduler.start()
